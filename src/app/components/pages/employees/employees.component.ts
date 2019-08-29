bffdcd69eff60a312da84c99161db2d06205e0c5
Joey Vinyard <jviny98@yahoo.com>
2019-08-29 09:09:45 -0400

    Work on employee fetching

--- src/app/components/pages/employees/employees.component.ts
+++ src/app/components/pages/employees/employees.component.ts
@@ -1,7 +1,7 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component } from '@angular/core';
 import { Employee } from 'src/app/models/employee';
 import { MatTableDataSource, MatPaginator } from '@angular/material';
-import { AngularFirestore } from '@angular/fire/firestore';
+import { LocationService } from 'src/app/services/location.service';
 
 @Component({
   selector: 'app-employees',
@@ -13,21 +13,16 @@ export class EmployeesComponent {
   employees: Employee[];
 
   dataSource = new MatTableDataSource<Employee>();
-  displayedColumns: string[] = ['firstName', 'lastName', 'action'];
-
-  @ViewChild(MatPaginator, { static: true }) paginator: MatPaginator;
+  displayedColumns: string[] = ['firstName', 'lastName', 'email', 'action'];
 
   manage(employee: Employee): void {
     console.log(employee);
   }
 
-  constructor(private afStorage: AngularFirestore) {
-    this.dataSource.paginator = this.paginator;
-
-    // let col = afStorage.collection("employees");
-    // this.employees.forEach((e) => {
-    //   col.add(e);
-    // })
+  constructor(private locationService: LocationService) {
+    locationService.status.subscribe(() => {
+      console.log("Finished");
+    });
   }
 
 }
--- src/app/services/location.service.ts
+++ src/app/services/location.service.ts
@@ -1,54 +1,60 @@
-import { Injectable } from '@angular/core';
+import { Injectable, EventEmitter } from '@angular/core';
 import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/firestore';
 import { Location } from '../models/location';
 import { UserInfo } from '../models/user-info';
 import { Employee } from '../models/employee';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
 export class LocationService {
-
-  private locationsCollection:AngularFirestoreCollection<Location> = this.afs.collection("locations");
-  private locations: Map<string, Location> = new Map();
-  private currentLocation: Location = null;
-  private currentLocationKey: string = null;
-
-  getLocations(): Map<string, Location> {
-    return this.locations;
-  }
-
-  getCurrentLocationKey(): string {
-    return this.currentLocationKey
-  }
-
-  getCurrentLocation(): Location {
-    return this.currentLocation;
-  }
-
-  setCurrentLocation(key: string): void {
-    this.currentLocation = this.locations.get(key);
-    this.currentLocationKey = key;
-  }
-
-  loadLocations(userInfo: UserInfo): void {
-    this.locations.clear();
-    userInfo.locations.forEach((location) => {
-      this.locationsCollection.doc<Location>(location).valueChanges().subscribe((locationData) => {
-        locationData.employees = new Map();
-        this.afs.collection<Employee>(`locations/${location}/employees`).snapshotChanges().subscribe((data) => {
-          data.forEach((d) => {
-            locationData.employees.set(d.payload.doc.id, d.payload.doc.data());
-          })
-        })
-        this.locations.set(location, locationData);
-        if(this.currentLocation == null) {
-          this.currentLocation = locationData;
-          this.currentLocationKey = location;
-        }
-      });
-    });
-  }
-
-  constructor(private afs: AngularFirestore) {}
+    
+    private locationsCollection:AngularFirestoreCollection<Location> = this.afs.collection("locations");
+    private locations: Map<string, Location> = new Map();
+    private currentLocation: Location = null;
+    private currentLocationKey: string = null;
+    public status: EventEmitter<void> = new EventEmitter();
+    
+    getLocations(): Map<string, Location> {
+        return this.locations;
+    }
+    
+    getCurrentLocationKey(): string {
+        return this.currentLocationKey
+    }
+    
+    getCurrentLocation(): Location {
+        return this.currentLocation;
+    }
+    
+    setCurrentLocation(key: string): void {
+        this.currentLocation = this.locations.get(key);
+        this.currentLocationKey = key;
+    }
+    
+    loadLocations(userInfo: UserInfo): void {
+        this.locations.clear();
+        let locationFetchStatus: Promise<void>[] = [];
+        locationFetchStatus.push(new Promise((res, rej) => {
+            userInfo.locations.forEach((location) => {
+                this.locationsCollection.doc<Location>(location).valueChanges().subscribe((locationData) => {
+                    locationData.employees = new Map();
+                    this.afs.collection<Employee>(`locations/${location}/employees`).snapshotChanges().subscribe((data) => {
+                        data.forEach((d) => {
+                            locationData.employees.set(d.payload.doc.id, d.payload.doc.data());
+                            this.locations.set(location, locationData);
+                            if(this.currentLocation == null) {
+                                this.currentLocation = locationData;
+                                this.currentLocationKey = location;
+                            }
+                            res();
+                        });
+                    });
+                });
+            });
+        }));
+        Promise.all(locationFetchStatus).then(() => this.status.emit());
+    }
+    
+    constructor(private afs: AngularFirestore) {}
 }
