2b8aaeebeef94db6c3721b0478a1220eec4e194c
JoeyVinyard <vinyardjoseph@gmail.com>
2019-08-30 13:53:58 -0400

    Employee fetching per location working

--- src/app/models/employee.ts
+++ src/app/models/employee.ts
@@ -1,6 +1,9 @@
-
 export interface Employee {
     firstName: string;
     lastName: string;
     email: string;
+}
+
+export interface DisplayedEmployee extends Employee {
+    id: string;
 }
--- src/app/services/location.service.ts
+++ src/app/services/location.service.ts
@@ -1,8 +1,9 @@
 import { Injectable, EventEmitter } from '@angular/core';
-import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/firestore';
+import { AngularFirestore, AngularFirestoreCollection, DocumentChangeAction } from '@angular/fire/firestore';
 import { Location } from '../models/location';
 import { UserInfo } from '../models/user-info';
 import { Employee } from '../models/employee';
+import { BehaviorSubject } from 'rxjs';
 
 @Injectable({
     providedIn: 'root'
@@ -13,47 +14,50 @@ export class LocationService {
     private locations: Map<string, Location> = new Map();
     private currentLocation: Location = null;
     private currentLocationKey: string = null;
-    public status: EventEmitter<void> = new EventEmitter();
+    public status: BehaviorSubject<boolean> = new BehaviorSubject(false);
     
-    getLocations(): Map<string, Location> {
+    public getLocations(): Map<string, Location> {
         return this.locations;
     }
     
-    getCurrentLocationKey(): string {
+    public getCurrentLocationKey(): string {
         return this.currentLocationKey
     }
     
-    getCurrentLocation(): Location {
+    public getCurrentLocation(): Location {
         return this.currentLocation;
     }
     
-    setCurrentLocation(key: string): void {
+    public setCurrentLocation(key: string): void {
         this.currentLocation = this.locations.get(key);
         this.currentLocationKey = key;
     }
     
-    loadLocations(userInfo: UserInfo): void {
+    private parseLocation(employeeData: DocumentChangeAction<Employee>, locationKey: string, locationData: Location): void {
+        locationData.employees.set(employeeData.payload.doc.id, employeeData.payload.doc.data());
+        this.locations.set(locationKey, locationData);
+        if (this.currentLocation == null) {
+            this.setCurrentLocation(locationKey);
+        }
+    }
+
+    public loadLocations(userInfo: UserInfo): void {
         this.locations.clear();
         let locationFetchStatus: Promise<void>[] = [];
         locationFetchStatus.push(new Promise((res, rej) => {
-            userInfo.locations.forEach((location) => {
-                this.locationsCollection.doc<Location>(location).valueChanges().subscribe((locationData) => {
+            userInfo.locations.forEach((locationKey) => {
+                this.locationsCollection.doc<Location>(locationKey).valueChanges().subscribe((locationData) => {
                     locationData.employees = new Map();
-                    this.afs.collection<Employee>(`locations/${location}/employees`).snapshotChanges().subscribe((data) => {
-                        data.forEach((d) => {
-                            locationData.employees.set(d.payload.doc.id, d.payload.doc.data());
-                            this.locations.set(location, locationData);
-                            if(this.currentLocation == null) {
-                                this.currentLocation = locationData;
-                                this.currentLocationKey = location;
-                            }
+                    this.afs.collection<Employee>(`locations/${location}/employees`).snapshotChanges().subscribe((employeeSnapshot) => {
+                        employeeSnapshot.forEach((employeeData) => {
+                            this.parseLocation(employeeData, locationKey, locationData);
                             res();
                         });
                     });
                 });
             });
         }));
-        Promise.all(locationFetchStatus).then(() => this.status.emit());
+        Promise.all(locationFetchStatus).then(() => this.status.next(true));
     }
     
     constructor(private afs: AngularFirestore) {}
