7c23da9f7e746601363211edc314b67431d88833
JoeyVinyard <vinyardjoseph@gmail.com>
2019-09-30 16:23:04 -0400

    Work on basic display of schedule

--- src/app/components/pages/schedule/schedule.component.ts
+++ src/app/components/pages/schedule/schedule.component.ts
@@ -3,6 +3,7 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { Schedule } from 'src/app/models/schedule';
 import { Location } from 'src/app/models/location';
 import { ScheduleService } from 'src/app/services/schedule.service';
+import { Sheet } from 'src/app/models/sheet';
 
 @Component({
   selector: 'app-schedule',
@@ -12,6 +13,9 @@ import { ScheduleService } from 'src/app/services/schedule.service';
 export class ScheduleComponent {
 
   private currentSchedule: Schedule;
+  private sheets: Sheet[];
+
+  private times: string[] = [];
 
   constructor(private scheduleService: ScheduleService, private activatedRoute: ActivatedRoute, private router: Router) {
     activatedRoute.paramMap.subscribe((map) => {
@@ -19,7 +23,14 @@ export class ScheduleComponent {
       this.scheduleService.loadSchedule(scheduleId);
       this.scheduleService.currentSchedule.subscribe((schedule) => {
         this.currentSchedule = schedule;
+        this.currentSchedule.loadSheets().subscribe((sheets) => {
+          this.sheets = sheets;
+          console.log(this.sheets);
+        });
       });
     });
+    for(let i = 7; i < 21; i++) {
+      this.times.push(i + ":00");
+    }
   }
 }
--- src/app/models/schedule.ts
+++ src/app/models/schedule.ts
@@ -1,9 +1,15 @@
 import { AngularFirestoreDocument } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+import { Sheet } from './sheet';
 
 export class Schedule {
     document: AngularFirestoreDocument;
     label: string;
     id: string;
+
+    public loadSheets(): Observable<Sheet[]> {
+        return this.document.collection<Sheet>("sheets").valueChanges();
+    }
 
     constructor(scheduleData: Schedule, document: AngularFirestoreDocument) {
         this.label = scheduleData.label;
