bb6ffac93bd708cb1dc964d3987cf89e0b87ca76
JoeyVinyard <vinyardjoseph@gmail.com>
2020-01-14 11:59:43 -0500

    Finished update to use spinner wheel for time selection

--- src/app/components/pages/schedule/new-sheet-dialog/new-sheet-dialog.component.ts
+++ src/app/components/pages/schedule/new-sheet-dialog/new-sheet-dialog.component.ts
@@ -3,6 +3,7 @@ import { FormControl, Validators } from '@angular/forms';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material';
 import { NewShiftDialogComponent } from '../new-shift-dialog/new-shift-dialog.component';
 import { Sheet } from 'src/app/models/sheet';
+import { TimeService } from 'src/app/services/time.service';
 
 @Component({
   selector: 'app-new-sheet-dialog',
@@ -12,9 +13,9 @@ import { Sheet } from 'src/app/models/sheet';
 export class NewSheetDialogComponent implements AfterViewInit {
 
   sheet: FormControl = new FormControl('', [Validators.required]);
-  open: FormControl = new FormControl('', [Validators.required]);
-  close: FormControl = new FormControl('', [Validators.required]);
-  timeIncrement: FormControl = new FormControl('', [Validators.required]);
+  open: FormControl = new FormControl('12:00 am', [Validators.required]);
+  close: FormControl = new FormControl('11:00 pm', [Validators.required]);
+  timeIncrement: FormControl = new FormControl('30', [Validators.required]);
 
   @ViewChild("openSheet", { static: true }) openField;
   @ViewChild("closeSheet", {static: true}) closeField;
@@ -28,8 +29,8 @@ export class NewSheetDialogComponent implements AfterViewInit {
   submit(): void {
     this.dialogRef.close({
         label: this.sheet.value,
-        openTime: this.open.value,
-        closeTime: this.close.value,
+        openTime: this.timeService.stringToTime(this.open.value),
+        closeTime: this.timeService.stringToTime(this.close.value),
         timeIncrement: this.timeIncrement.value
       } as Sheet
     );
@@ -67,11 +68,14 @@ export class NewSheetDialogComponent implements AfterViewInit {
     }
   }
 
-  constructor(@Inject(MAT_DIALOG_DATA) public data: {sheet:Sheet}, public dialogRef: MatDialogRef<NewShiftDialogComponent>) {
+  constructor(
+    public dialogRef: MatDialogRef<NewShiftDialogComponent>,
+    public timeService: TimeService,
+    @Inject(MAT_DIALOG_DATA) public data: {sheet:Sheet}) {
     if(data.sheet) {
       this.sheet.setValue(data.sheet.label);
-      this.open.setValue(data.sheet.openTime);
-      this.close.setValue(data.sheet.closeTime);
+      this.open.setValue(timeService.timeToString(data.sheet.openTime));
+      this.close.setValue(timeService.timeToString(data.sheet.closeTime));
       this.timeIncrement.setValue(data.sheet.timeIncrement);
     }
   }
