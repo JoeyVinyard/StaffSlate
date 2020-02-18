eb2a3d39677fecb5c3ebec0dd29f8007def576d5
JoeyVinyard <vinyardjoseph@gmail.com>
2020-02-18 15:42:06 -0500

    Work on automatically deleting employee shifts when employee is deleted

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -8,3 +8,32 @@ export const createViewId = functions.firestore.document("locations/{locationId}
     .onCreate((snapshot, context) => {
         return snapshot.ref.set({"viewId": uniqid()}, {merge: true});
     });
+
+export const deleteEmployeeShifts = functions.firestore.document("locations/{locationId}/employees/{employeeId}")
+    .onDelete((snapshot, context) => {
+        const location = snapshot.ref.parent.parent;
+        const empId = snapshot.id;
+        if(location) {
+            location.collection("schedules").listDocuments().then((scheduleRefs) => {
+                scheduleRefs.forEach((scheduleRef) => {
+                    scheduleRef.collection("sheets").listDocuments().then((sheetRefs) => {
+                        sheetRefs.forEach((sheetRef) => {
+                            sheetRef.collection("shifts").where("empId","==",empId).get().then((shifts) => {
+                                shifts.forEach((shift) => {
+                                    shift.ref.delete().catch((err) => {
+                                        throw err;
+                                    });
+                                });
+                            }).catch((err) => {
+                                throw err;
+                            });
+                        });
+                    }).catch((err) => {
+                        throw err;
+                    });
+                });
+            }).catch((err) => {
+                throw err;
+            });
+        }
+    });
