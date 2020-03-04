0cfb91a3c1ab259da4f92ceadc3db71f0e16bf9a
JoeyVinyard <vinyardjoseph@gmail.com>
2020-03-04 12:48:44 -0500

    Work on settings page

--- src/app/components/pages/location/location.component.ts
+++ src/app/components/pages/location/location.component.ts
@@ -1,23 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy } from '@angular/core';
 import { LocationService } from 'src/app/services/location.service';
 import { ActivatedRoute } from '@angular/router';
+import { Location } from 'src/app/models/location';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-location',
   templateUrl: './location.component.html',
   styleUrls: ['./location.component.css']
 })
-export class LocationComponent implements OnInit {
+export class LocationComponent implements OnDestroy {
+
+  public curLocation: Location;
+  private sub: Subscription;
+
+  public formatAddress(): string {
+    if(this.curLocation) {
+      return `${this.curLocation.address}`;
+    }
+    return "";
+  }
 
   constructor(private route: ActivatedRoute, public locationService: LocationService) {
     route.params.subscribe((params) => {
       const locationId = params.locationId;
-      locationService.loadLocation(locationId);
-      
+      this.sub = locationService.loadLocation(locationId).subscribe((location) => {
+        this.curLocation = location;
+      });
     });
   }
 
-  ngOnInit() {
+  ngOnDestroy() {
+    this.sub && this.sub.unsubscribe();
   }
 
 }
