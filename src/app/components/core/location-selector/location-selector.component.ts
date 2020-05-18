9880b1976087638b02f84fd2697b1d0413b87ae5
JoeyVinyard <vinyardjoseph@gmail.com>
2020-05-17 21:53:28 -0400

    Set last accessed location

--- src/app/components/core/location-selector/location-selector.component.ts
+++ src/app/components/core/location-selector/location-selector.component.ts
@@ -1,18 +1,31 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { LocationService } from 'src/app/services/location.service';
 import { Location } from 'src/app/models/location';
+import { Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
+import { UserService } from 'src/app/services/user.service';
 
 @Component({
   selector: 'app-location-selector',
   templateUrl: './location-selector.component.html',
   styleUrls: ['./location-selector.component.css']
 })
-export class LocationSelectorComponent {
+export class LocationSelectorComponent implements OnDestroy {
 
+  alive: Subject<boolean> = new Subject<boolean>();
   locations: [string, Location][];
 
-  constructor(public locationService: LocationService) {
-    this.locationService.getLocationsMap().subscribe((locationMap) => {
+  public select(locationId: string): void {
+    this.userService.setLastAccessedLocation(locationId);
+    this.locationService.loadLocation(locationId)
+  }
+
+  ngOnDestroy() {
+    this.alive.next(true);
+  }
+
+  constructor(private locationService: LocationService, private userService: UserService) {
+    this.locationService.getLocationsMap().pipe(takeUntil(this.alive)).subscribe((locationMap) => {
       this.locations = Array.from(locationMap.entries());
     });
   }
