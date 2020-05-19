6c3832f5bed413b6a86c199c1cb6089734d34a32
Joey Vinyard <vinyardjoseph@gmail.com>
2020-05-19 00:52:47 -0400

    Created seperate environment for development (#10)

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,11 +1,13 @@
 export const environment = {
   production: false,
   firebase: {
-    apiKey: 'AIzaSyCxjdOhuPbpDgbFfKEaLFa8dsgM3KmV3ZU',
-    authDomain: 'emsys-39a0f.firebaseapp.com',
-    databaseURL: 'https://emsys-39a0f.firebaseio.com',
-    projectId: 'emsys-39a0f',
-    storageBucket: 'emsys-39a0f.appspot.com',
-    messagingSenderId: '964364848205'
+    apiKey: "AIzaSyB1_nq2nU5nuUfFAwO3Qfqw4ogHAgI0yRo",
+    authDomain: "ems-dev-afdf5.firebaseapp.com",
+    databaseURL: "https://ems-dev-afdf5.firebaseio.com",
+    projectId: "ems-dev-afdf5",
+    storageBucket: "ems-dev-afdf5.appspot.com",
+    messagingSenderId: "1089782226863",
+    appId: "1:1089782226863:web:e9167e36085def2b6cd95a",
+    measurementId: "G-5W4YX2ESHE"
   }
 };
