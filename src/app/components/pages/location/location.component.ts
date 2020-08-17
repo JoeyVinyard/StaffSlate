b4012b68441ce655137d23bae6fafd5836ad8fd4
JoeyVinyard <vinyardjoseph@gmail.com>
2020-08-16 20:12:47 -0400

    locations css rewrite

--- src/app/components/pages/location/location.component.ts
+++ src/app/components/pages/location/location.component.ts
@@ -1,46 +1,79 @@
-import { Component, OnDestroy } from '@angular/core';
-import { LocationService } from 'src/app/services/location.service';
-import { ActivatedRoute } from '@angular/router';
-import { Location } from 'src/app/models/location';
-import { FormControl, Validators } from '@angular/forms';
-import { MatSnackBar } from '@angular/material/snack-bar';
-import { switchMap, takeWhile } from 'rxjs/operators';
+import { Component, OnDestroy } from "@angular/core";
+import { LocationService } from "src/app/services/location.service";
+import { ActivatedRoute } from "@angular/router";
+import { Location } from "src/app/models/location";
+import { FormControl, Validators } from "@angular/forms";
+import { MatSnackBar } from "@angular/material/snack-bar";
+import { switchMap, takeWhile, takeUntil } from "rxjs/operators";
+import { Subject } from "rxjs";
 
 @Component({
-  selector: 'app-location',
-  templateUrl: './location.component.html',
-  styleUrls: ['./location.component.css']
+    selector: "app-location",
+    templateUrl: "./location.component.html",
+    styleUrls: ["./location.component.css"],
 })
 export class LocationComponent implements OnDestroy {
+    public editing: boolean = false;
+    public curLocation: Location;
+    private alive: Subject<boolean> = new Subject<boolean>();
 
-  public editingLabel: boolean = false;
-  public editingAddress: boolean = false;
-  public curLocation: Location;
-  private alive: boolean = true;
+    public label = new FormControl("", [Validators.required]);
+    public address = new FormControl("", [Validators.required]);
+    public city = new FormControl("", [Validators.required]);
+    public state = new FormControl("", [Validators.required]);
+    public zip = new FormControl("", [Validators.required]);
 
-  public label = new FormControl('', [Validators.required]);
+    public editLocation(): void {
+        this.editing = true;
+    }
 
-  public saveLabel(): void {
-    this.editingLabel = false;
-    this.curLocation.document.update({label: this.label.value}).then(() => {
-      this.snackbar.open("Location Name successfully updated!", "Dismiss", {duration: 2000});
-    }).catch((err) => {
-      this.snackbar.open("Failed to update Location Name!", "Dismiss", {duration: 2000});
-    });
-  }
+    public saveLocation(): void {
+        this.editing = false;
+        this.curLocation.document
+            .update({
+                label: this.label.value,
+                address: this.address.value,
+                city: this.city.value,
+                state: this.state.value,
+                zip: this.zip.value,
+            })
+            .then(() => {
+                this.snackbar.open("Location Name successfully updated!", "Dismiss", {
+                    duration: 2000,
+                });
+            })
+            .catch((err) => {
+                this.snackbar.open("Failed to update Location Name!", "Dismiss", {
+                    duration: 2000,
+                });
+            });
+    }
 
-  constructor(private route: ActivatedRoute, public locationService: LocationService, private snackbar: MatSnackBar) {
-    route.params.pipe(
-      switchMap((params) => locationService.loadLocation(params.locationId)),
-      takeWhile(() => this.alive)
-    ).subscribe((location) => {
-      this.curLocation = location;
-      this.label.setValue(location.label);
-    });
-  }
+    public getAddress(): string {
+        return `${this.curLocation.address}, ${this.curLocation.city} ${this.curLocation.state}, ${this.curLocation.zip}`;
+    }
 
-  ngOnDestroy() {
-    this.alive = false;
-  }
+    constructor(
+        private route: ActivatedRoute,
+        public locationService: LocationService,
+        private snackbar: MatSnackBar
+    ) {
+        route.params
+            .pipe(
+                switchMap((params) => locationService.loadLocation(params.locationId)),
+                takeUntil(this.alive)
+            )
+            .subscribe((location) => {
+                this.curLocation = location;
+                this.label.setValue(location.label);
+                this.address.setValue(location.address);
+                this.city.setValue(location.city);
+                this.state.setValue(location.state);
+                this.zip.setValue(location.zip);
+            });
+    }
 
+    ngOnDestroy() {
+        this.alive.next(true);
+    }
 }
