76486213e84dea3a2b657e651285b4a43fa92fc4
JoeyVinyard <vinyardjoseph@gmail.com>
2020-08-16 20:15:19 -0400

    refactored ts

--- src/app/components/pages/location/location.component.ts
+++ src/app/components/pages/location/location.component.ts
@@ -2,9 +2,7 @@ import { Component, OnDestroy } from "@angular/core";
 import { LocationService } from "src/app/services/location.service";
 import { ActivatedRoute } from "@angular/router";
 import { Location } from "src/app/models/location";
-import { FormControl, Validators } from "@angular/forms";
-import { MatSnackBar } from "@angular/material/snack-bar";
-import { switchMap, takeWhile, takeUntil } from "rxjs/operators";
+import { switchMap, takeUntil } from "rxjs/operators";
 import { Subject } from "rxjs";
 
 @Component({
@@ -13,51 +11,14 @@ import { Subject } from "rxjs";
     styleUrls: ["./location.component.css"],
 })
 export class LocationComponent implements OnDestroy {
-    public editing: boolean = false;
     public curLocation: Location;
     private alive: Subject<boolean> = new Subject<boolean>();
-
-    public label = new FormControl("", [Validators.required]);
-    public address = new FormControl("", [Validators.required]);
-    public city = new FormControl("", [Validators.required]);
-    public state = new FormControl("", [Validators.required]);
-    public zip = new FormControl("", [Validators.required]);
-
-    public editLocation(): void {
-        this.editing = true;
-    }
-
-    public saveLocation(): void {
-        this.editing = false;
-        this.curLocation.document
-            .update({
-                label: this.label.value,
-                address: this.address.value,
-                city: this.city.value,
-                state: this.state.value,
-                zip: this.zip.value,
-            })
-            .then(() => {
-                this.snackbar.open("Location Name successfully updated!", "Dismiss", {
-                    duration: 2000,
-                });
-            })
-            .catch((err) => {
-                this.snackbar.open("Failed to update Location Name!", "Dismiss", {
-                    duration: 2000,
-                });
-            });
-    }
 
     public getAddress(): string {
         return `${this.curLocation.address}, ${this.curLocation.city} ${this.curLocation.state}, ${this.curLocation.zip}`;
     }
 
-    constructor(
-        private route: ActivatedRoute,
-        public locationService: LocationService,
-        private snackbar: MatSnackBar
-    ) {
+    constructor(private route: ActivatedRoute, public locationService: LocationService) {
         route.params
             .pipe(
                 switchMap((params) => locationService.loadLocation(params.locationId)),
@@ -65,11 +26,6 @@ export class LocationComponent implements OnDestroy {
             )
             .subscribe((location) => {
                 this.curLocation = location;
-                this.label.setValue(location.label);
-                this.address.setValue(location.address);
-                this.city.setValue(location.city);
-                this.state.setValue(location.state);
-                this.zip.setValue(location.zip);
             });
     }
 
